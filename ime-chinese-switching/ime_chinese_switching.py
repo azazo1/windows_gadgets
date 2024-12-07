fa3ee4aa7611f132ee7934849df332c0a49dfa1f
azazo1 <azazo1@qq.com>
2024-12-07 12:38:01 +0800

    ime-chinese-switching - 重写原理, 使用 pywin32 而不是模拟键盘操作

--- ime-chinese-switching/ime_chinese_switching.py
+++ ime-chinese-switching/ime_chinese_switching.py
@@ -1,122 +1,79 @@
-import logging
+"""
+参考自: https://github.com/PEMessage/im-select-imm/blob/imm/src/im-select-imm.cpp
+"""
+import ctypes
 import time
-import traceback
+import win32con
+import win32gui
+import win32process
+from win32api import GetKeyboardLayout, PostMessage, SendMessage
 
-import pynput.keyboard
-import uiautomation
+# NI_CONTEXTUPDATED 查看 immdev.h 发现以下内容, 但是在 learn microsoft 文档中没有记录:
+IMC_SETCONVERSIONMODE = 0x0002
+IMC_SETSENTENCEMODE = 0x0004
+IMC_SETOPENSTATUS = 0x0006
 
-_taskbar = uiautomation.PaneControl(Depth=1, Name="任务栏", ClassName="Shell_TrayWnd")
 
-
-class ReserveCursorFocus:
+# 获取当前输入法布局（返回低 16 位的布局标识符）
+def get_input_method():
     """
-    在执行某个操作后恢复鼠标指针位置和焦点.
-
-    Examples:
-
-    >>> # 1
-    >>> with ReserveCursorFocus():
-    ...     print("Hello World") # 替换成其他会改变鼠标位置或者焦点的操作.
-    Hello World
-    >>> # 2
-    >>> reserve = ReserveCursorFocus()
-    >>> reserve.save() # 此行可选, 也可调用多次.
-    >>> print("Hello World") # 替换成其他会改变鼠标位置或者焦点的操作.
-    Hello World
-    >>> reserve.restore() # 此行可调用多次.
+    Returns:
+        - 1033 是英文输入法.
+        - 2052 是微软拼音输入法.
     """
-
-    def __init__(self, reserve_cursor=True, reserve_focus=True):
-        """
-        Parameters:
-            reserve_cursor: 是否保存鼠标位置.
-            reserve_focus: 是否保存焦点.
-        """
-        self.reserve_cursor = reserve_cursor
-        self.reserve_focus = reserve_focus
-        self.cursor: tuple[int, int] = (-1, -1)
-        self.focus: int = -1
-        self.save()
-
-    def save(self):
-        self.cursor = uiautomation.GetCursorPos()
-        self.focus = uiautomation.GetForegroundWindow()
-
-    def restore(self):
-        if self.reserve_focus:
-            uiautomation.ControlFromHandle(self.focus).SetFocus()
-        if self.reserve_cursor:
-            uiautomation.SetCursorPos(*self.cursor)
-        time.sleep(0.1)  # 确保焦点已经完全转移, 否则再次调用 GetForegroundWindow 会返回 None.
-
-    def __enter__(self):
-        self.save()
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        self.restore()
+    hwnd = win32gui.GetForegroundWindow()
+    if hwnd:
+        thread_id, _ = win32process.GetWindowThreadProcessId(hwnd)
+        # 获取当前线程的键盘布局（HKL）
+        current_layout = GetKeyboardLayout(thread_id)
+        layout_id = current_layout & 0x0000FFFF
+        return layout_id
+    return None
 
 
-def get_taskbar() -> uiautomation.PaneControl:
+def switch_input_mode(mode):
+    if mode < 0:
+        return
+    foreground_window = win32gui.GetForegroundWindow()
+    foreground_ime = ctypes.windll.imm32.ImmGetDefaultIMEWnd(foreground_window)
+    if foreground_ime:
+        SendMessage(foreground_ime, win32con.WM_IME_CONTROL, IMC_SETCONVERSIONMODE, mode)
+
+
+def get_input_mode():
     """
-    获取任务栏控件.
+    API: https://learn.microsoft.com/en-us/previous-versions/aa913780(v=msdn.10)
+    对于 Microsoft 旧版中文输入法（Windows 10 及之前）:
+          0: 英文
+          1: 中文
+    对于 Microsoft 新版中文输入法（Windows 11）:
+          0: 英文 / 半角
+          1: 中文 / 半角
+          1024: 英文 / 全角（Bit10 和 Bit1 用于表示）
+          1025: 中文 / 全角
     """
-    _taskbar.Refind(0, 0)
-    return _taskbar
+    foreground_window = win32gui.GetForegroundWindow()
+    foreground_ime = ctypes.windll.imm32.ImmGetDefaultIMEWnd(foreground_window)
+    if foreground_ime:
+        result = SendMessage(foreground_ime, win32con.WM_IME_CONTROL, 0x01, 0)
+        return result
+    return None
 
 
-def get_ms_ch_ime_button() -> uiautomation.Control:
-    """
-    获取微软拼音输入法中文输入法按钮控件.
-
-    - 如果输入法语言只有一种, 此按钮不会出现, 返回 None.
-    - 如果当前输入法不是微软系统中文输入法, 返回 None.
-    """
-
-    def cmp_func(control: uiautomation.Control, depth: int):
-        return all((
-            isinstance(control, uiautomation.ButtonControl),
-            control.ClassName == "SystemTray.NormalButton",
-            control.AutomationId == "SystemTrayIcon",
-            depth == 3,
-            control.Name == '托盘输入指示器 中文(简体，中国)\r\n微软拼音\r\n\r\n要切换输入法，按 Windows 键+空格。'
-        ))
-
-    return uiautomation.FindControl(get_taskbar(), cmp_func, 3)  # FindControl 不会缓存搜索, 都是直接搜索.
-
-
-def get_en_mode_button() -> uiautomation.Control:
-    """
-    获取微软拼音输入法英语模式按钮.
-    """
-
-    def cmp_func(control: uiautomation.Control, depth: int):
-        return all((
-            isinstance(control, uiautomation.ButtonControl),
-            control.ClassName == "SystemTray.NormalButton",
-            control.AutomationId == "SystemTrayIcon",
-            depth == 3,
-            control.Name == '托盘输入指示器 英语模式\n\n单击右键以查看更多选项'
-        ))
-
-    return uiautomation.FindControl(get_taskbar(), cmp_func, 3)
+def switch_input_method(locale):
+    if locale < 0:
+        return
+    hwnd = win32gui.GetForegroundWindow()
+    PostMessage(hwnd, win32con.WM_INPUTLANGCHANGEREQUEST, 0, locale)
 
 
 def main():
-    controller = pynput.keyboard.Controller()
-    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                        level=logging.DEBUG)
     while True:
-        try:
-            ime = get_ms_ch_ime_button()
-            en = get_en_mode_button()
-            if ime and en:
-                controller.press(pynput.keyboard.Key.ctrl_l)
-                time.sleep(0.1)
-                controller.release(pynput.keyboard.Key.ctrl_l)
-                logging.info("switched")
-            time.sleep(0.4)
-        except Exception:
-            logging.error(traceback.format_exc())
+        method = get_input_method()
+        mode = get_input_mode()
+        if method == 2052 and mode & 0x01 == 0:
+            switch_input_mode(1)
+        time.sleep(0.1)
 
 
 if __name__ == '__main__':
